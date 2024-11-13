02115a34c33e02b0d5a7cb8479149387039538a5
MoeMoeKyun <92113036+2xMoeKyun@users.noreply.github.com>
2024-11-13 17:39:17 +0300

    Car Gameplay

---
+++ Assets/sprites/Street/CarController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 5f;  // Скорость движения
+    public Vector2 moveDirection = Vector2.left; // Направление движения (к игроку)
+
+    void Update()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+        transform.localScale += Vector3.one*(speed/10000);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Проверка на столкновение с игроком
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/sprites/Street/CarSpawner.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour
+{
+    public List<GameObject> cars;
+    public List<Transform> spawnPoints;
+
+
+    private void Start()
+    {
+        StartCoroutine(Spawning(new List<Transform>() { spawnPoints[0], spawnPoints[1] }, 0));
+        StartCoroutine(Spawning(new List<Transform>() { spawnPoints[2], spawnPoints[3] }, 2));
+        StartCoroutine(Spawning(new List<Transform>() { spawnPoints[4] }, 4));
+    }
+
+    private IEnumerator Spawning(List<Transform> points, int trimIndex)
+    {
+        while (true)
+        {
+            GameObject randomCar = cars[Random.Range(0, cars.Count)];
+            Transform randomSpawn = spawnPoints[Random.Range(trimIndex, points.Count+trimIndex)];
+
+            randomSpawn.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 1);
+            yield return new WaitForSeconds(1.5f);
+            GameObject car = Instantiate(randomCar, randomSpawn.position, randomCar.transform.rotation);
+            randomSpawn.gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 0, 0, 0);
+
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
+            StartCoroutine(DestroyCar(car));
+        }
+    }
+    private IEnumerator DestroyCar(GameObject car)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(car);
+    }
+}
